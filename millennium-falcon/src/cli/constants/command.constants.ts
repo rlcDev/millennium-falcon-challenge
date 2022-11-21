cccb1df1542597246bd1131c10d79b268c2f49e6
laurent <dev.rlc974@gmail.com>
2022-11-21 23:25:23 +0400

    Complete cli

---
+++ millennium-falcon/src/cli/constants/command.constants.ts
@@ -0,0 +1,6 @@
+export const INVALID_MILLENNIUM_FILE =
+  'Millennium Falcon file content is not correct';
+export const MISSING_PATHS = 'Please provide the falcon and empire paths';
+export const MISSING_EMPIRE = 'Please provide the empire path';
+export const TOO_MUCH_ARGUMENTS = 'Too much arguments';
+export const ERROR = 'An error occurred';
