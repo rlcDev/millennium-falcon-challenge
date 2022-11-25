efc1e371e22ac77ffd42479164c4580a386a2bbd
rlcDev <dev.rlc974@gmail.com>
2022-11-25 10:37:57 +0400

    chore: Headless chrome

--- c3po/src/app/on-board-computer/components/computer/cont/on-board-computer-cont.component.spec.ts
+++ c3po/src/app/on-board-computer/components/computer/cont/on-board-computer-cont.component.spec.ts
@@ -1,8 +1,9 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {OnBoardComputerContComponent} from './on-board-computer-cont.component';
-import {OddMissionService} from "../../../services/odd-mission/odd-mission.service";
-import {HttpClientModule} from "@angular/common/http";
+import { OnBoardComputerContComponent } from './on-board-computer-cont.component';
+import { OddMissionService } from '../../../services/odd-mission/odd-mission.service';
+import { HttpClientModule } from '@angular/common/http';
+import { OnBoardComputerPresComponent } from './pres/on-board-computer-pres.component';
 
 describe('OnBoardComputerContComponent', () => {
   let component: OnBoardComputerContComponent;
@@ -11,10 +12,12 @@ describe('OnBoardComputerContComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      declarations: [OnBoardComputerContComponent],
-      providers: [OddMissionService, OddMissionService]
-    })
-      .compileComponents();
+      declarations: [
+        OnBoardComputerContComponent,
+        OnBoardComputerPresComponent,
+      ],
+      providers: [OddMissionService, OddMissionService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(OnBoardComputerContComponent);
     component = fixture.componentInstance;
