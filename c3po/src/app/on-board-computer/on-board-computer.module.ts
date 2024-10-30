f6bc41c9929cc8280b05d9aa6fa25ce90e8a0ee6
laurent <dev.rlc974@gmail.com>
2024-10-30 22:55:16 +0100

    tech(c3po): Migrate to Angular 18

--- c3po/src/app/on-board-computer/on-board-computer.module.ts
+++ c3po/src/app/on-board-computer/on-board-computer.module.ts
@@ -3,23 +3,19 @@ import { CommonModule } from "@angular/common";
 import { OnBoardComputerContComponent } from "./components/computer/cont/on-board-computer-cont.component";
 import { OnBoardComputerPresComponent } from "./components/computer/cont/pres/on-board-computer-pres.component";
 import { OddMissionService } from "./services/odd-mission/odd-mission.service";
-import { HttpClientModule } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     OnBoardComputerContComponent,
     OnBoardComputerPresComponent
   ],
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ],
-  providers: [
-    OddMissionService
-  ],
   exports: [
     OnBoardComputerContComponent,
     OnBoardComputerPresComponent
+  ], imports: [CommonModule], providers: [
+    OddMissionService,
+    provideHttpClient(withInterceptorsFromDi())
   ]
 })
 export class OnBoardComputerModule {
